6e769a8f0501ec164384aa62e81883eae1557bd1
khushipunjabi22 <128999136+khushipunjabi22@users.noreply.github.com>
2023-11-05 22:58:25 +0530

    Update adjacencymatrix.c

--- adjacencymatrix.c
+++ adjacencymatrix.c
@@ -1,69 +1,37 @@
+#include <stdio.h>
+#define MAX_NODES 100
+int adjMatrix[MAX_NODES][MAX_NODES];
+void addEdge(int from, int to) {
+adjMatrix[from][to] = 1;
+adjMatrix[to][from] = 1;
+}
 int main() {
- int size;
- printf("Enter number of nodes:");
- scanf("%d", &size);
- int arr[size][size];
- int choice, exit=1,m,n;
- char p,q;
- int i,j;
- for(i = 0;i<size;i++){
- for(j=0;j<size;j++){
- arr[i][j] = 0;
- }
- }
- while(exit == 1){
- printf("\n\nEnter an Option\n1.Add an Directed edge\n2.Add an Undirected edge\n3. Show
-Adjacency Matrix\n4. Quit\n");
- scanf("%d", &choice);
- switch(choice){
- case 1:
- printf("Enter the name of node(A to Z) from which you want to add an directed edge to other
-node(A to Z):");
- scanf(" %c %c", &p,&q);
- m = p - 65;
- n = q - 65;
- if(m>=size || n >=size || m == n){
- printf("\nCannot add an edge\n");
- }
- else{
- arr[m][n] = 1;
- printf("Successfully added edge from %c to %c\n", p, q);
- }
- break;
- case 2:
- printf("Enter the name of node(A to Z) from which you want to add an directed edge to other
-node(A to Z):");
- scanf(" %c %c", &p,&q);
- m = p - 65;
- n = q - 65;
- if(m>=size || n >=size || m == n){
- printf("\nCannot add an edge\n");
- }
- else{
- arr[m][n] = 1;
- arr[n][m] = 1;
- printf("Successfully added edge between %c and %c\n", p, q);
- }
- break;
- case 3:
- printf("The adjacency matrix is:\n");
- printf(" ");
- for(j=0;j<size;j++){
- printf("%c ", j+65);
- }
- printf("\n");
- for(i=0;i<size;i++){
- printf("%c ", i+65);
- for(j=0;j<size;j++){
- printf("%d ", arr[i][j]);
- }
- printf("\n");
- }
- break;
- case 4:
- exit = 0;
- break;
- }
- }
- return 0;
+int numNodes, numEdges;
+printf("Enter the number of nodes: ");
+scanf("%d", &numNodes);
+printf("Enter the number of edges: ");
+scanf("%d", &numEdges);
+for (int i = 0; i < numNodes; i++) {
+for (int j = 0; j < numNodes; j++) {
+adjMatrix[i][j] = 0;
+}
+}
+for (int i = 0; i < numEdges; i++) {
+int from, to;
+printf("Enter edge %d (from to): ", i + 1);
+scanf("%d %d", &from, &to);
+if (from < 0 || from >= numNodes || to < 0 || to >= numNodes) {
+printf("Invalid node numbers. Edge ignored.\n");
+continue;
+}
+addEdge(from, to);
+}
+printf("\nAdjacency Matrix:\n");
+for (int i = 0; i < numNodes; i++) {
+for (int j = 0; j < numNodes; j++) {
+printf("%d ", adjMatrix[i][j]);
+}
+printf("\n");
+}
+return 0;
 }
